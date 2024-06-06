17a97d54c3cd63a593163d42da39901b3adf9558
Archanajyothy <archanajyothy@gmail.com>
2024-06-06 14:30:28 +0530

    tagsbar created

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,5 +1,5 @@
+import { Tag } from './app/shared/models/Tag';
 import {Food} from './app/shared/models/food';
-// import { Tag } from './app/shared/models/Tag';
 
 export const sample_foods: Food[] = [
   {
@@ -136,13 +136,13 @@ export const sample_foods: Food[] = [
   },
 ]
 
-// export const sample_tags:Tag[] = [
-//   { name: 'All', count: 6 },
-//   { name: 'FastFood', count: 4 },
-//   { name: 'Pizza', count: 2 },
-//   { name: 'Lunch', count: 3 },
-//   { name: 'SlowFood', count: 2 },
-//   { name: 'Hamburger', count: 1 },
-//   { name: 'Fry', count: 1 },
-//   { name: 'Soup', count: 1 },
-// ]
+export const sample_tags:Tag[] = [
+  { name: 'All', count: 6 },
+  { name: 'FastFood', count: 4 },
+  { name: 'Pizza', count: 2 },
+  { name: 'Lunch', count: 3 },
+  { name: 'SlowFood', count: 2 },
+  { name: 'Hamburger', count: 1 },
+  { name: 'Fry', count: 1 },
+  { name: 'Soup', count: 1 },
+]
