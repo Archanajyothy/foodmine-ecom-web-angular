b1def52858435d98f3f218e0f15661673d71ff46
Archanajyothy <archanajyothy@gmail.com>
2024-06-12 10:24:32 +0530

    Login page jwt token created and designed the UI

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -144,3 +144,21 @@ export const sample_tags:any[] = [
   { name: 'Fry', count: 2 },
   { name: 'Soup', count: 2 },
 ]
+
+export const sample_users: any[] = [
+  {
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    password: '12345',
+    address: 'Toronto On',
+    isAdmin: true,
+  },
+  {
+    name: 'Jane Doe',
+    email: 'Jane@gmail.com',
+    password: '12345',
+    address: 'Shanghai',
+    isAdmin: false,
+  },
+];
+
